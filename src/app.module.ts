4bfe22d339e2e878ca5317a5d31ccc503dc37af6
tsegaye27 <ttalegn29@gmail.com>
2025-03-10 13:03:36 +0300

    feat(auth): integrate AuthModule into AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TasksModule, UsersModule],
+  imports: [TasksModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
